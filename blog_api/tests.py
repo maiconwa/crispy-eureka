c50309a9d4093d4f41aef607f0f0abdc71387428
Maicon William <maicon.w.a@gmail.com>
2022-03-16 00:27:48 -0300

    testing blog API

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from blog.models import Post, Category
 
-# Create your tests here.
+class PostTests(APITestCase):
+    
+    def test_view_posts(self):
+        
+        url = reverse('blog_api:listcreate')
